6476dc9488665ea0251f0153111b6f115d43b42e
Oleksandr Kravchuk <oakravchuk@luxoft.com>
2020-05-28 20:06:47 +0300

    added shading in view space

--- basic_lightning_view_space.cpp
+++ basic_lightning_view_space.cpp
@@ -1,24 +1,24 @@
 #include "functions.h"
 #include <iostream>
 
-void mouse_callback6(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback6(GLFWwindow* window, double xoffset, double yoffset);
-void processInput6(GLFWwindow* window);
+void mouse_callback7(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback7(GLFWwindow* window, double xoffset, double yoffset);
+void processInput7(GLFWwindow* window);
 
 // camera
-Camera camera6(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX6 = SCR_WIDTH / 2.0f;
-float lastY6 = SCR_HEIGHT / 2.0f;
-bool firstMouse6 = true;
+Camera camera7(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX7 = SCR_WIDTH / 2.0f;
+float lastY7 = SCR_HEIGHT / 2.0f;
+bool firstMouse7 = true;
 
 // timing
-float deltaTime6 = 0.0f;
-float lastFrame6 = 0.0f;
+float deltaTime7 = 0.0f;
+float lastFrame7 = 0.0f;
 
 // lighting
-glm::vec3 lightPos6(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos7(1.2f, 1.0f, 2.0f);
 
-int main_move()
+int main()
 {
     GLFWwindow* window;
     try {
@@ -27,13 +27,13 @@ int main_move()
     catch (std::exception& e) {
         return -1;
     }
-    glfwSetCursorPosCallback(window, mouse_callback6);
-    glfwSetScrollCallback(window, scroll_callback6);
+    glfwSetCursorPosCallback(window, mouse_callback7);
+    glfwSetScrollCallback(window, scroll_callback7);
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glEnable(GL_DEPTH_TEST);
 
-    Shader lightingShader("basic_lightning_specular.vs", "basic_lightning_specular.fs");
+    Shader lightingShader("basic_lightning_view.vs", "basic_lightning_view.fs");
     Shader lightCubeShader("light_cube.vs", "light_cube.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
@@ -117,32 +117,28 @@ int main_move()
         // per-frame time logic
         // --------------------
         float currentFrame = glfwGetTime();
-        deltaTime6 = currentFrame - lastFrame6;
-        lastFrame6 = currentFrame;
+        deltaTime7 = currentFrame - lastFrame7;
+        lastFrame7 = currentFrame;
 
         // input
         // -----
-        processInput6(window);
+        processInput7(window);
 
         // render
         // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
-        lightPos6.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-        lightPos6.y = sin(glfwGetTime() / 2.0f) * 1.0f;
-
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("lightPos", lightPos6);
-        lightingShader.setVec3("viewPos", camera6.Position);
+        lightingShader.setVec3("lightPos", lightPos7);
+        lightingShader.setVec3("viewPos", camera7.Position);
 
         // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(camera6.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera6.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera7.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera7.GetViewMatrix();
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
 
@@ -160,7 +156,7 @@ int main_move()
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
         model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos6);
+        model = glm::translate(model, lightPos7);
         model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
         lightCubeShader.setMat4("model", model);
 
@@ -188,44 +184,44 @@ int main_move()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput6(GLFWwindow* window)
+void processInput7(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera6.ProcessKeyboard(FORWARD, deltaTime6);
+        camera7.ProcessKeyboard(FORWARD, deltaTime7);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera6.ProcessKeyboard(BACKWARD, deltaTime6);
+        camera7.ProcessKeyboard(BACKWARD, deltaTime7);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera6.ProcessKeyboard(LEFT, deltaTime6);
+        camera7.ProcessKeyboard(LEFT, deltaTime7);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera6.ProcessKeyboard(RIGHT, deltaTime6);
+        camera7.ProcessKeyboard(RIGHT, deltaTime7);
 }
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
-void mouse_callback6(GLFWwindow* window, double xpos, double ypos)
+void mouse_callback7(GLFWwindow* window, double xpos, double ypos)
 {
-    if (firstMouse6)
+    if (firstMouse7)
     {
-        lastX6 = xpos;
-        lastY6 = ypos;
-        firstMouse6 = false;
+        lastX7 = xpos;
+        lastY7 = ypos;
+        firstMouse7 = false;
     }
 
-    float xoffset = xpos - lastX6;
-    float yoffset = lastY6 - ypos; // reversed since y-coordinates go from bottom to top
+    float xoffset = xpos - lastX7;
+    float yoffset = lastY7 - ypos; // reversed since y-coordinates go from bottom to top
 
-    lastX6 = xpos;
-    lastY6 = ypos;
+    lastX7 = xpos;
+    lastY7 = ypos;
 
-    camera6.ProcessMouseMovement(xoffset, yoffset);
+    camera7.ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
-void scroll_callback6(GLFWwindow* window, double xoffset, double yoffset)
+void scroll_callback7(GLFWwindow* window, double xoffset, double yoffset)
 {
-    camera6.ProcessMouseScroll(yoffset);
+    camera7.ProcessMouseScroll(yoffset);
 }
